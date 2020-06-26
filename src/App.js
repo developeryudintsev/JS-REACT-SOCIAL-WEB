4fe7385a361d9427ff25241d41b44fcd591868cc
login <kakvospitat@gmail.com>
2020-06-26 11:07:08 +0300

    'tdd-25.06.2020'

--- src/App.js
+++ src/App.js
@@ -3,25 +3,20 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import { Route} from "react-router-dom";
 import Header from "./components/Header/Header";
 
 const App = (props) => {
     return (
-        <BrowserRouter>
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
                 <div class='app-wrapper-content'>
-                    <Route path='/dialogs' render={() =>
-                        <Dialogs state={props.state.dialogsPage}
-                        />}/>
-                    <Route path='/profile' render={() =>
-                        <Profile state={props.state.profilePage}
-                        />}/>
+                    <Route path='/dialogs' render={() => <Dialogs state={props.state.dialogsPage}/>}/>
+                    <Route path='/profile' render={() => <Profile state={props.state.profilePage}/>}/>
                 </div>
             </div>
-        </BrowserRouter>);
+        );
 }
 
 export default App;
---
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from "./../Dialogs.module.css";
+import {NavLink} from 'react-router-dom';
+
+const DialogItem = (props) => {
+    return (
+        <div className={s.active}>
+            <NavLink to={'/dialogs/' + props.id} className={s.LinkD}>
+                {props.name}
+            </NavLink>
+        </div>
+    )
+}
+
+
+export default DialogItem;
